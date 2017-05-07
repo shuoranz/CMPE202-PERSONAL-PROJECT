b523f6c805dc7bf80cbbe07562954928e84b8d05
ShuoranZ <shuoranz@compandsave.com>
2017-05-07 01:27:52 -0700

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 //import java.util.Optional;
 import java.util.Scanner;
@@ -23,34 +24,21 @@ public class Main {
 	private static final Object STATIC = null;
 
 	public static void main(String[] args) throws IOException{
-		
-
 		//TestCase1();
 		//TestCase2();
 		//TestCase3();
 		TestCase4();
 		//TestCase5();
 		//TestHello();
-		/*
-		try {
-		    File file = new File("src/Hello1.java");
-		    Scanner scanner = new Scanner(file);
-		    String lines;
-		    while(scanner.hasNextLine()){
-		    	lines = scanner.nextLine();
-		    	//System.out.println(lines);
-		    }
-		    scanner.close();
-		    
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		}
-		*/
-        
 	}
 	
 	public static class MethodVisitor extends VoidVisitorAdapter<Void> {
         private String result;
+        private String classDependency = "";
+        private boolean depFlag = false;
+        private ArrayList<String> allClass = new ArrayList();
+        private String currentClassName;
+        private String currentClassType;
         public String getParseResult(){
         	String returnVal = this.result;
         	if(returnVal != null && !returnVal.isEmpty()){
@@ -60,32 +48,84 @@ public class Main {
         	}
         	
         }
+        public void setAllClasses(ArrayList<String> allClasses){
+        	int length = allClasses.size();
+        	for(int i=0; i<length; i++){
+        		this.allClass.add(allClasses.get(i));
+        	}
+        }
+        public String getClassDependency(){
+        	String returnVal = this.classDependency;
+        	if(returnVal != null && !returnVal.isEmpty()){
+        		return this.classDependency;
+        	} else {
+        		return "";
+        	}
+        }
+        public void setFlag(){
+        	this.depFlag = true;
+        }
+        public void setCurrentClass(String[] CCN){
+        	this.currentClassName = CCN[0];
+        	this.currentClassType = CCN[1];
+        }
         private void setParseResult(String value){this.result+=value+"\n";}
+        private void setClassDependency(String value){this.classDependency+=value+"\n";}
         @Override
         public void visit(MethodDeclaration n, Void arg) {
-
-        	//System.out.print(n.getType() + " " + n.getModifiers());
-        	//System.out.println(n.toString());
-        	String modifier = n.getModifiers().toString();
-        	if(modifier.equals("[PUBLIC]")){
-        		
-        		NodeList paras = n.getParameters();
-	        	int length = paras.size();
-	        	String resultPara;
-	        	resultPara = "";
-	        	for(int i=0;i<length;i++){
-	        		String tempPara = paras.get(i).toString();
-	        		String[] splitPara = tempPara.split(" ");
-	        		//System.out.println(tempPara);
-	        		resultPara += splitPara[1] + " : " + splitPara[0] + ",";
-	        	}
-	        	if (resultPara != null && resultPara.length() > 0) {
-	        		resultPara = resultPara.substring(0, resultPara.length()-1);
-        	    }
-        		this.setParseResult( "+" + n.getName().toString() + "(" + resultPara + "): " + n.getType().toString());
+        	if(this.depFlag == false){
+        		String modifier = n.getModifiers().toString();
+            	if(modifier.equals("[PUBLIC]")){
+            		
+            		NodeList paras = n.getParameters();
+    	        	int length = paras.size();
+    	        	String resultPara;
+    	        	resultPara = "";
+    	        	for(int i=0;i<length;i++){
+    	        		String tempPara = paras.get(i).toString();
+    	        		String[] splitPara = tempPara.split(" ");
+    	        		resultPara += splitPara[1] + " : " + splitPara[0] + ",";
+    	        	}
+    	        	if (resultPara != null && resultPara.length() > 0) {
+    	        		resultPara = resultPara.substring(0, resultPara.length()-1);
+            	    }
+            		this.setParseResult( "+" + n.getName().toString() + "(" + resultPara + "): " + n.getType().toString());
+            	}
+        	} else {
+        		String modifier = n.getModifiers().toString();
+        		String temp1 = "";
+            	if(modifier.equals("[PUBLIC]")){
+            		NodeList paras = n.getParameters();
+    	        	int length = paras.size();
+    	        	String resultPara;
+    	        	resultPara = "";
+    	        	for(int i=0;i<length;i++){
+    	        		String tempPara = paras.get(i).toString();
+    	        		String[] splitPara = tempPara.split(" ");
+    	        		resultPara += splitPara[1] + " : " + splitPara[0] + ",";
+    	        		int length1 = this.allClass.size();
+    	        		for(int j=0; j<length1; j++){
+    	        			if(allClass.get(j).equals(splitPara[0]) && currentClassType.equals("false")){
+    	        				
+    	        				temp1 = this.currentClassName + " ..> " + splitPara[0]+"\n";
+    	        				if(this.getClassDependency().equals("")){
+    	        					System.out.println("fisrt");
+    	        					this.setClassDependency(temp1);
+    	        				}else if(!this.getClassDependency().contains(temp1)){
+    	        					this.setClassDependency(this.getClassDependency()+temp1);
+    	        				}
+    	        			}
+    	        		}
+    	        	}
+    	        	//System.out.println(temp1);
+    	        	if (resultPara != null && resultPara.length() > 0) {
+    	        		resultPara = resultPara.substring(0, resultPara.length()-1);
+            	    }
+            	}
         	}
+        	
             //super.visit(n, arg);
-            
+
         }
         
     }
@@ -102,7 +142,7 @@ public class Main {
         //plantUmlSource.append("skinparam classAttributeIconSize 0\n");
         plantUmlSource.append("class Hello1 {\n");
         String temp = visitor.getParseResult().replace("null","");
-        System.out.println(temp);
+        //System.out.println(temp);
         plantUmlSource.append(temp + " \n");
         //plantUmlSource.append("String getStr() \n");
         
@@ -239,14 +279,9 @@ public class Main {
 	}
 	
 	static boolean TestCase4() throws IOException{
-		/*
-		FileInputStream in = new FileInputStream("src/A.java");
-        CompilationUnit cu = JavaParser.parse(in);
-        MethodVisitor visitor = new MethodVisitor();
-        visitor.visit(cu, null);
-        */
 		File folder = new File("src/test4");
 		File[] listOfFiles = folder.listFiles();
+		ArrayList<String> allClasses = new ArrayList<String>();
 		String valClass = "@startuml\n";
 	    for (int i = 0; i < listOfFiles.length; i++) {
 	      if (listOfFiles[i].isFile()) {
@@ -274,6 +309,7 @@ public class Main {
 	        //System.out.println(temp);
 	        valClass += temp + "\n}\n";
 	        //System.out.println("}");
+	        allClasses.add(cu.getTypes().get(0).getNameAsString());
 	      }
 	    }
 	    
@@ -285,12 +321,23 @@ public class Main {
 		        //System.out.println("File " + fileName);
 		        FileInputStream in = new FileInputStream("src/test4/"+fileName);
 		        CompilationUnit cu = JavaParser.parse(in);
-		        valClassRelation += getClassRelation(cu);
+		        valClassRelation += getClassRelation(cu,allClasses);
+		        MethodVisitor visitor = new MethodVisitor();
+		        visitor.setAllClasses(allClasses);
+		        visitor.setFlag();
+		        String currentClassName = cu.getTypes().get(0).getNameAsString();
+		        String[] CNN = {
+		        	currentClassName,
+		        	isInterface(cu,currentClassName).toString()
+		        };
+		        visitor.setCurrentClass(CNN);
+		        visitor.visit(cu, null);
+		        valClassRelation += visitor.getClassDependency();
 	    	}
 	    }
 	    valClass += valClassRelation;
 	    valClass += "@enduml";
-	    System.out.println(valClass);
+	    //System.out.println(valClass);
 		StringBuilder plantUmlSource = new StringBuilder();
 		/*
         String temp = "@startuml\n"
@@ -413,7 +460,6 @@ public class Main {
 		//String className = compilationUnit.getTypes().get(0).getName().toString();
 		String className = compilationUnit.getTypes().get(0).getNameAsString();
 		String returnValue;
-		//compilationUnit.getTypes().get(0).getFields().toString()
 		if(!isInterface(compilationUnit,className)){
 			returnValue = "class " + className + "{\n";
 		} else {
@@ -427,15 +473,55 @@ public class Main {
 		return compilationUnit.toString().contains("interface "+className);
 	}
 	
-	private static String getFather(CompilationUnit compilationUnit, String className){
-		return null;
-	}
-	
-	private static String getClassRelation(CompilationUnit cu){
+	private static String getClassRelation(CompilationUnit cu, ArrayList<String> allClasses){
 		
 		String className = cu.getTypes().get(0).getNameAsString();
 		String cutDeclaration = cutDeclaration(cu,className);
-		return cutDeclaration;
+		String cutField = cutField(cu,className,allClasses);
+		return cutDeclaration+cutField;
+	}
+	
+	private static String cutField(CompilationUnit cu, String className, ArrayList<String> allClasses){
+		ArrayList<String> allFieldRelation = new ArrayList<String>();
+		List Fields = cu.getTypes().get(0).getFields();
+		String returnValue = "";
+		for(int i=0; i<Fields.size(); i++){
+			String temp1 = Fields.get(i).toString();
+			if(temp1.contains("=")){
+				String[] temp2 = temp1.split("=");
+				for(int j=0; j<allClasses.size(); j++){
+					if((temp2[0].contains(" "+ allClasses.get(j) +" ") || temp2[0].contains("<"+ allClasses.get(j) +">"))
+							&&
+						(temp2[1].contains(" "+ allClasses.get(j) +" ") || temp2[1].contains("<"+ allClasses.get(j) +">"))){
+						// x to multiple
+						
+						allFieldRelation.add(className + " \"1\" -- \"many\" " + allClasses.get(j));
+					}
+				}
+			} else if(temp1.contains("[]")){
+				for(int j=0; j<allClasses.size(); j++){
+					if(temp1.contains(" "+ allClasses.get(j) +" ") || temp1.contains(" "+ allClasses.get(j) +"[]")){
+						// x to multiple
+						
+						allFieldRelation.add(className + " \"1\" -- \"many\" " + allClasses.get(j));
+					}
+				}
+			} else {
+				for(int j=0; j<allClasses.size(); j++){
+					if(temp1.contains(" "+ allClasses.get(j) +" ")){
+						// x to multiple
+						
+						allFieldRelation.add(className + " -- " + allClasses.get(j));
+					}
+				}
+			}
+			//System.out.println(Fields.get(i));
+			for(int k=0; k<allFieldRelation.size(); k++){
+				//System.out.println(allFieldRelation.get(k));
+				returnValue += allFieldRelation.get(k) + "\n";
+			}
+		}
+		return returnValue;
 	}
 	
 	private static String cutDeclaration(CompilationUnit cu, String className){
@@ -454,7 +540,6 @@ public class Main {
 				} else {
 					returnValue += className + " --|> " + temp3[0] + "\n";
 				}
-				//System.out.println(returnValue);
 			} else if (containsIgnoreCase(temp2[1],"implements")){
 				String[] temp3 = temp1[0].split(" implements ");
 				if(temp3[1].contains(" ")){
@@ -463,7 +548,6 @@ public class Main {
 				} else {
 					returnValue += className + " ..|> " + temp3[0] + "\n";
 				}
-				//System.out.println(returnValue);
 			}
 		} else {
 			return "";
